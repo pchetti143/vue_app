b8ee383228670d04ae9952daf76a5ef40fd87152
Chetti Praneeth <pchetti@opentext.com>
2022-09-10 19:36:17 +0530

    if-else

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,29 +1,15 @@
 <template>
 <h1>Radio and CheckBox</h1>
-<label for="java">Java</label>
-<input type="checkbox" id="java" value="java" v-model="technology" /><br>
-<label for="php">PHP</label>
-<input type="checkbox" id="php" value="php" v-model="technology" /><br>
-<label for="python">python</label>
-<input type="checkbox" id="python" value="python" v-model="technology" /><br>
-<h2>{{technology}}</h2>
-<br><br>
-<h2>Radio Button for profession</h2>
-<label for="stu">Student</label>
-<input type="radio" id="stu" value="stu" v-model="profession"/><br>
-<label for="dev">Developer</label>
-<input type="radio" id="dev" value="dev" v-model="profession"/><br>
-<h2>profession :{{profession}}</h2>
+<h4 v-if="show">Condition true</h4>
+<h4 v-else>Condition False</h4>
+<button v-on:click="show=!show">Click me</button>
 </template>
 
 <script>
 export default {
     name: 'HomePage',
-    data() {
-        return {
-            technology: [],
-            profession:[]
-        }
+    data(){
+        return {show:true}
     }
 }
 </script>
