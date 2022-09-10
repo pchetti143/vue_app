06137984467df0d43475a3bed706ad82362e838d
Chetti Praneeth <pchetti@opentext.com>
2022-09-10 22:02:26 +0530

    reuse component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,44 @@
 <template>
 <h1>child-compo test</h1>
-<Child :fname="name" :cardata="cars"/>
+<h2 v-for="item in users" :key="item"><User :data="item" :getdata="getdata" /></h2>
 </template>
 
 <script>
-import Child from './Child.vue';
+import User from './User.vue';
 
 export default {
     name: "HomePage",
-    components: { Child },
-    data(){
-         return{
-            name:"chetti",
-            cars:{1:"audi",2:"BMW",3:"Benz"}
-         }
+    components: {
+        User
+    },methods:{
+        getdata(x){
+            console.log("Congratulation " + x)
+        }
+    },
+    data() {
+        return {
+            users: [{
+                    name: "anil",
+                    email: "anil@test.com"
+                },
+                {
+                    name: "sam",
+                    email: "sam@test.com"
+                },
+                {
+                    name: "rey",
+                    email: "rey@test.com"
+                },
+                {
+                    name: "hey",
+                    email: "hey@test.com"
+                },
+                {
+                    name: "pit",
+                    email: "pit@test.com"
+                }
+            ]
+        }
     }
 }
 </script>
---
+++ src/components/User.vue
@@ -0,0 +1,13 @@
+<template>
+    <h6>Name is : {{data.name}} <br> My email is : {{data.email}}</h6>
+    <button v-on:click="getdata(data.name)">Click me </button>
+</template>
+<script>
+     export default{
+        name:"User-Page",
+        props:{
+            data:Object,
+            getdata:Function
+        }
+     }
+</script>
