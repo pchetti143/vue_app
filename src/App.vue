5732ca5954e5a63b94b02e394e3c077b160732a3
Chetti Praneeth <pchetti@opentext.com>
2022-09-10 14:11:54 +0530

    5 commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <img alt="Vue logo" src="./assets/logo.png">
-<HelloWorld msg="Welcome to Ganesha _techpark_ Palace" />
+<HelloWorld msg="Welcome to Ganesha _techpark domulur_ Palace" />
 </template>
 
 <script>
